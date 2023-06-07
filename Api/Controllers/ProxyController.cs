cf7928fec00ecc8218f37fa0205e50b95044bc9b
deleteLater <mikcczhang@gmail.com>
2023-06-07 20:34:01 +0800

    add ProxyController placeholder

---
+++ Api/Controllers/ProxyController.cs
@@ -0,0 +1,32 @@
+using Api.Persistence;
+using Api.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class ProxyController : ApiControllerBase
+{
+    private readonly IRepository _repository;
+
+    public ProxyController(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("status")]
+    public IActionResult GetStatusAsync()
+    {
+        var status = new
+        {
+            lastSyncTime = DateTime.UtcNow
+        };
+
+        return new JsonResult(status);
+    }
+
+    [HttpPost("bootstrap")]
+    public IActionResult BootstrapAsync()
+    {
+        return Ok();
+    }
+}
