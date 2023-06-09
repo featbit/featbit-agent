7b8f89a5d6ba8161c6a873c33cf1c8e3af5157e7
deleteLater <mikcczhang@gmail.com>
2023-06-09 22:10:10 +0800

    make datetime to always come back as utc kind

--- Api/Persistence/FbDbContext.cs
+++ Api/Persistence/FbDbContext.cs
@@ -1,6 +1,7 @@
 using Api.Store;
 using Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Api.Persistence;
 
@@ -12,6 +13,23 @@ public class FbDbContext : DbContext
 
     public FbDbContext(DbContextOptions<FbDbContext> builderOptions) : base(builderOptions)
     {
+    }
+
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder
+            .Properties<DateTime>()
+            .HaveConversion<UtcValueConverter>();
+    }
+
+    // ref: https://github.com/dotnet/efcore/issues/4711#issuecomment-1048572602
+    class UtcValueConverter : ValueConverter<DateTime, DateTime>
+    {
+        public UtcValueConverter()
+            : base(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+        {
+        }
     }
 
     public void EnsureTableCreated()
