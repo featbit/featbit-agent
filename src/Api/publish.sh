355af5e83419c77f7bf80eb3438272bd786fbb10
deleteLater <mikcczhang@gmail.com>
2023-06-14 19:35:20 +0800

    fix publish path

--- src/Api/publish.sh
+++ src/Api/publish.sh
@@ -11,6 +11,6 @@ rm -rf "$publishBaseDir"/*
 for platform in "${platforms[@]}"; do
   dotnet publish -r "$platform" -c Release -p:DebugType=none --self-contained true -o "$publishBaseDir/$platform" || exit 1
   cd "$publishBaseDir/$platform" || exit 1
-  tar -czvf "./featbit_agent_${platform}_${version}.tar.gz" ./* -C ../ || exit 1
+  tar -czvf "../featbit_agent_${platform}_${version}.tar.gz" ./* || exit 1
   cd ../../
 done
